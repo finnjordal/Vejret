af0eb085fc74819e3da00389640be9a85e945ce6
Finn Jordal <finn@jordal.dk>
2013-02-26 13:26:01 +0100

    klar

--- vejretapp.js
+++ vejretapp.js
@@ -7,7 +7,7 @@ var express= require('express')
 var app= express()
 	, server = require('http').createServer(app)
   , io = io.listen(server)
-  ,	port= 9000;
+  ,	port= 3000;
 
 function buildInternUrl(postnr, dage) {
 	return '/images/' + postnr + '/' + dage + '/byvejr.gif';
